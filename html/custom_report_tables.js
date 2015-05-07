b3b415f3c4c1762df7cd6a83f8a89df4ec931fa6
ichuang <ichuang@mit.edu>
2015-05-07 13:50:30 -0400

    custom reports tables js lib - add parameters for dom, searching, width

--- html/custom_report_tables.js
+++ html/custom_report_tables.js
@@ -3,14 +3,14 @@
     optarg = optarg || {};
     ntables += 1;
     var div_id = "table-" + report_name + "-" + ntables;
-    var html = '<table id="' + div_id + '" class="display" width="100%"></table>';
+    var html = '<table id="' + div_id + '" class="display" width="' + (optarg.width || '100%') + '"></table>';
     report_div.append(html);
     // console.log('tablecolumns=', tablecolumns, ', tabledata=', tabledata);
     var table = $('#' + div_id).DataTable({
-      dom: 'T<"clear">lfrtip',
+      dom: optarg.dom==null ? 'T<"clear">lfrtip' : optarg.dom,
       "columns": tablecolumns,
       "pageLength": 10,
-      searching: true,
+      searching: optarg.searching==null ? true : optarg.searching,
       search:{"regex": true},
       ordering: optarg.ordering==null ? true : optarg.ordering,
       paging: optarg.paging==null ? true : optarg.paging,
