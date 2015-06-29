9eec1f3a83b7451f568015a67e94f2edc0241a1f
ichuang <ichuang@mit.edu>
2015-06-29 19:01:09 -0400

    add /file/get/report  and  /file/list/report in storage.py

--- storage.py
+++ storage.py
@@ -4,26 +4,32 @@ import json
 import webapp2
 import cloudstorage as gcs
 import local_config
+import unicodecsv as csv
 
 from google.appengine.api import app_identity
 import auth
 from auth import auth_required, auth_and_role_required
 from templates import JINJA_ENVIRONMENT
+from datasource import DataSource
 from stats import DataStats
+from collections import OrderedDict
 
-class FileStoragePages(auth.AuthenticatedHandler, DataStats):
+# for local dev debugging, use "gsutil -d ls" and copy str after "Bearer"
+# gcs.common.set_access_token("ya...")
+
+class FileStoragePages(auth.AuthenticatedHandler, DataStats, DataSource):
     '''
     Provide access to files in google cloud storage, stored
     using the convention of directories named by course_id.replace('/', '__').
     
     Limit access to content in the "DIST" subdirectory of the course directory.
     '''
-    def course_bucket(self, course_id, filename=None):
+    def course_bucket(self, course_id, filename=None, ddir="/DIST"):
         try:
             gs_bucket = local_config.GOOGLE_STORAGE_ROOT
         except Exception as err:
             gs_bucket = app_identity.get_default_gcs_bucket_name()
-        gsfp = "/%s/%s/DIST" % (gs_bucket, course_id.replace('/', '__'))
+        gsfp = "/%s/%s%s" % (gs_bucket, course_id.replace('/', '__'), ddir)
         if filename:
             gsfp += "/" + filename
         return gsfp
@@ -51,9 +57,26 @@ class FileStoragePages(auth.AuthenticatedHandler, DataStats):
 
         bucket = self.course_bucket(course_id)
         fn_filter = self.request.get('filter', None)
+        return self.list_files_available_in_bucket(bucket, fn_filter)
 
+    @auth_and_role_required(role='pm')
+    @auth_required
+    def ajax_list_report_files(self):
+        '''
+        Return json with list of files avilable in reports
+        '''
+        if not self.user in self.AUTHORIZED_USERS:	# require superuser
+            return self.no_auth_sorry()
+
+        cr_dir = self.get_course_report_dataset()
+        bucket = self.course_bucket(cr_dir, ddir="")
+        return self.list_files_available_in_bucket(bucket)
+
+    @auth_required
+    def list_files_available_in_bucket(self, bucket, fn_filter=None):
         files = []
         logging.info("list bucket = %s" % bucket)
+        # logging.info("gcs access token = %s" % gcs.common.get_access_token())
         #for stat in gcs.listbucket(bucket + '/b', delimiter='/'):
         for stat in gcs.listbucket(bucket):
             fn = stat.filename
@@ -87,8 +110,66 @@ class FileStoragePages(auth.AuthenticatedHandler, DataStats):
             self.response.out.write({"error": str(err)})
             return
 
+    @auth_required
+    def ajax_get_report(self, filename=None, group_tag=None):
+        '''
+        Return JSON of data from specified CSV file.  Only include courses which have a matching tag
+        (in the "tags" column of the course listings).  Control access based on group_tag.
+
+        The file must be in the course_report or course_report_latest bucket.
+        '''
+        filename = self.request.get('filename', None)
+        if not filename:
+            self.response.headers['Content-Type'] = 'application/json'   
+            self.response.out.write({"error": "No filename specified"})
+            return
+        if not filename.endswith('.csv'):
+            self.response.headers['Content-Type'] = 'application/json'   
+            self.response.out.write({"error": "Only CSV files can be specified"})
+            return
+        
+        group_tag = group_tag or self.request.get('group_tag', None)
+
+        if not group_tag:
+            if not self.does_user_have_role('pm'):
+                return self.no_auth_sorry()
+        else:
+            if not self.is_user_authorized_for_course(group_tag):
+                return self.no_auth_sorry()
+
+        courses = self.get_course_listings(check_individual_auth=False)
+        known_course_ids_with_tags = {x['course_id']: x.get('tags', None) for x in courses['data']}
+
+        try:
+            cr_dir = self.get_course_report_dataset()
+            bucket = self.course_bucket(cr_dir, filename, ddir="")
+            gcs_file = gcs.open(bucket)
+            data_by_cid = OrderedDict()
+
+            for row in csv.DictReader(gcs_file):
+                if not 'course_id' in row:
+                    self.response.headers['Content-Type'] = 'application/json'   
+                    self.response.out.write({"error": "CSV file missing course_id column"})
+                    return
+                cid = row['course_id']
+                if not cid in known_course_ids_with_tags:
+                    continue
+                course_tags = known_course_ids_with_tags[cid]
+                if not self.course_listings_row_has_tag(course_tags, group_tag):
+                    continue
+                data_by_cid[cid] = row
+                
+            self.response.write(json.dumps(data_by_cid))
+            gcs_file.close()
+        except Exception as err:
+            self.response.headers['Content-Type'] = 'application/json'   
+            self.response.out.write({"error": str(err)})
+            return
+
 FileStorageRoutes = [
 # ajax routes
     webapp2.Route('/file/list/course/<org>/<number>/<semester>', handler=FileStoragePages, handler_method='ajax_list_files_available'),
     webapp2.Route('/file/get/course/<org>/<number>/<semester>', handler=FileStoragePages, handler_method='ajax_get_file'),
+    webapp2.Route('/file/list/report', handler=FileStoragePages, handler_method='ajax_list_report_files'),
+    webapp2.Route('/file/get/report', handler=FileStoragePages, handler_method='ajax_get_report'),
 ]
